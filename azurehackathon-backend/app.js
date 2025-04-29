ce60441ef9d0f63e811886aaebe9ebe312ac9f6a
ridomaulana12 <Rido.Maulana@sera.astra.co.id>
2025-04-30 01:36:38 +0700

    text hehe

--- azurehackathon-backend/app.js
+++ azurehackathon-backend/app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -9,6 +9,7 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const chatAgentRouter = require("./routes/chatAgent");
+const proposalAgentRouter = require("./routes/proposalAgentRouter");
 
 const app = express();
 app.use(cors());
@@ -27,19 +28,16 @@ app.use(express.json());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/chat-agent", chatAgentRouter);
+app.use("/proposal", proposalAgentRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
